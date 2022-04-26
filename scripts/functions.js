45359e0055d15707a412263c924b5163ce1b85eb
KotlovAlex <103385044+KotlovAlex@users.noreply.github.com>
2022-04-26 22:36:45 +0700

    SSA
    SSA

--- scripts/functions.js
+++ scripts/functions.js
@@ -104,9 +104,9 @@ function createNewMatrix(row, column) {
 function trajMatr (arr,len) {
     let temparr = strToInt(arr);
     let L = len
-    let res = new Array(temparr.length-L+1)
-    for (let i = 0; i<res.length;i++){
-        res[i] = (temparr.slice(i,i+L))
+    let res = new Array(L)
+    for (let i = 0; i<L;i++){
+        res[i] = (temparr.slice(i,i+temparr.length-L))
     }
     return res
 }
@@ -123,27 +123,68 @@ function transMatr (matr) {
 }
 
 //Matrix multiplication
-
 function multiMatr(matrA,matrB) {
-    let A = matrA.slice();
-    let B = matrB.slice();
-    A = transMatr(A);
-    B = transMatr(B);
+    return math.multiply(matrA,matrB)
+    // let A = matrA.slice();
+    // let B = matrB.slice();
+    // A = transMatr(A);
+    // B = transMatr(B);
     
-    m = A[0].length
-    q = B.length
-    n = A.length
+    // m = A[0].length
+    // q = B.length
+    // n = A.length
     
-    let C = [...Array(m)].map(e => Array(q));
-    for (let i = 0; i<m;i++){
-        for (let j = 0; j< q;j++){
-            C[i][j]=0;
-            for (let k = 0; k < n; k++){
-                C[i][j] += A[k][i] * B[j][k]
-            }
+    // let C = [...Array(m)].map(e => Array(q));
+    // for (let i = 0; i<m;i++){
+    //     for (let j = 0; j< q;j++){
+    //         C[i][j]=0;
+    //         for (let k = 0; k < n; k++){
+    //             C[i][j] += A[k][i] * B[j][k]
+    //         }
+    //     }
+    // }
+    // return C
+}
+
+//Scalar multiplication (multiplication of a matrix by a number)
+function scMulMatr (matr, number) {
+    res = matr.slice()
+    for (let i = 0; i<res[0].length;i++){
+        for (let j = 0; j<res.length;j++){
+            res[j][i]*=number
         }
     }
-    return C
+    return res
+}
+
+//Scalar division (division of a matrix by a number)
+function scDivMatr (matr, number) {
+    res = matr.slice()
+    for (let i = 0; i<res[0].length;i++){
+        for (let j = 0; j<res.length;j++){
+            res[j][i]/=number
+        }
+    }
+    return res
+}
+
+//Singular value decomposition (SVD)
+function svd (opens, len) {
+    let X = trajMatr(opens, len);
+    let XT = transMatr(X);
+    let temp = math.eigs(multiMatr(X,XT))
+    let values = temp.values
+    let vectors =  temp.vectors
+    let V = new Array(50)
+    let res = new Array(50)
+    for (let i = 0; i<len;i++){
+        V[i]=math.multiply(multiMatr(XT,vectors[i]),Math.sqrt(values[i]))
+    }
+    let VT = transMatr(V);
+    for (let i = 0; i<len;i++){
+        res[i] = multiMatr(math.multiply(Math.sqrt(values[i]),vectors[i]),VT[i])
+    }
+    console.log([X,res]);
 }
 
 compsel.onchange = () => {
